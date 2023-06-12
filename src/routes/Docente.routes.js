8286b76ee885031c031b3eebb8d4deccdba573aa
Franx Junior <franklinjunior021118@gmail.com>
2023-06-11 23:26:19 -0500

    este si

--- src/routes/Docente.routes.js
+++ src/routes/Docente.routes.js
@@ -1,13 +1,13 @@
 const Docente = require('express').Router();
-const { getDocentes, getDocente, posDocente , updaDocente ,deleteDocente,IniciarSeccion}= require('../controller/docente.controller')
+import { getDocentes, getDocente, posDocente, updaDocente, deleteDocente, IniciarSeccion } from '../controller/docente.controller';
 
 
 Docente.get('/docentes',getDocentes)
-Docente.get('/docente/:id',getDocente)
+Docente.get('/docente/:id',getDocente)git 
 Docente.post('/docentes',posDocente)
 Docente.put('/docente/:id',updaDocente)
 Docente.delete('/docente/:id',deleteDocente)
 
 Docente.post('/iniciarseccion',IniciarSeccion)
 
-module.exports= Docente
+export default Docente
